4ee86222b0a26e5bc5564a68ef71c65399cb00e7
Haram Chang <harammchang@gmail.com>
2021-09-21 13:05:29 -0400

    created Weather model

---
+++ server/db/models/Weather.js
@@ -0,0 +1,31 @@
+const Sequelize = require("sequelize")
+const db = require("./db")
+
+const Weather = db.define("Weather", {
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  localTime: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  temperature: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+  },
+  feels: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  },
+  humidity: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  text: {
+    type: Sequelize.STRING
+  }
+})
+
+module.exports = Weather
